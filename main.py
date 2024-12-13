7391f85fb13bd14c0ff087d1f98a7e7acb69776e
Omar Alshammari <omar.alshammary@hotmail.com>
2024-12-13 18:50:18 +0300

    fix drawing issue

--- main.py
+++ main.py
@@ -7,46 +7,17 @@ from qiskit import QuantumCircuit
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-global_numb_qubits = 100
-
-def simulate_bb84_without_eve(num_qubits):
-    qc = QuantumCircuit(global_numb_qubits)
-    alice = Alice(global_numb_qubits, qc)
-    bob = Bob(global_numb_qubits, qc)
+def simulate_bb84_without_eve(qc, num_qubits):
+    alice = Alice(num_qubits, qc)
+    bob = Bob(num_qubits, qc)
     # Call prepare_qubits explicitly
-    alice.prepare_qubits()
-
-    bob.measure_qubits()
-
-    alice_sifted_bits, alice_matching_indices = alice.sift_key(bob.bases)
-    print(alice_matching_indices)
-    bob_sifted_bits = bob.sift_key(alice_matching_indices)
-
-    print(alice_sifted_bits)
-    print(bob_sifted_bits)
-    print("Alice's sifted key count is:" + str(len(alice_sifted_bits)))
-    print("Bob's sifted key count is:" + str(len(bob_sifted_bits)))
-
-    if alice_sifted_bits == bob_sifted_bits:
-        print("Key exchange was successful.")
-    else:
-        print("Key exchange failed.")
-
-    qc.draw("mpl")
-
-def simulate_bb84_with_eve(num_qubits):
-    qc = QuantumCircuit(global_numb_qubits)
-    alice = Alice(global_numb_qubits, qc)
-    bob = Bob(global_numb_qubits, qc)
-    eve = Eve(global_numb_qubits, qc)
-    # Call prepare_qubits explicitly
+    qc.barrier(label="Alice - Start")
     alice.prepare_qubits()
     qc.barrier(label="Alice - End")
 
-    eve.measure_qubits()
-    qc.barrier(label="Eve - End")
-
+    qc.barrier(label="Bob - Start")
     bob.measure_qubits()
+    qc.barrier(label="Bob - End")
 
     alice_sifted_bits, alice_matching_indices = alice.sift_key(bob.bases)
     print(alice_matching_indices)
@@ -64,6 +35,35 @@ def simulate_bb84_with_eve(num_qubits):
 
     qc.draw("mpl")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    simulate_bb84_without_eve(100)
+def simulate_bb84_with_eve(qc, num_qubits):
+    alice = Alice(num_qubits, qc)
+    bob = Bob(num_qubits, qc)
+    eve = Eve(num_qubits, qc)
+    # Call prepare_qubits explicitly
+    qc.barrier(label="Alice - Start")
+    alice.prepare_qubits()
+    qc.barrier(label="Alice - End")
+
+    qc.barrier(label="Eve - Start")
+    eve.measure_qubits()
+    qc.barrier(label="Eve - End")
+
+    qc.barrier(label="Bob - Start")
+    bob.measure_qubits()
+    qc.barrier(label="Bob - End")
+    
+    alice_sifted_bits, alice_matching_indices = alice.sift_key(bob.bases)
+    print(alice_matching_indices)
+    bob_sifted_bits = bob.sift_key(alice_matching_indices)
+
+    print(alice_sifted_bits)
+    print(bob_sifted_bits)
+    print("Alice's sifted key count is:" + str(len(alice_sifted_bits)))
+    print("Bob's sifted key count is:" + str(len(bob_sifted_bits)))
+
+    if alice_sifted_bits == bob_sifted_bits:
+        print("Key exchange was successful.")
+    else:
+        print("Key exchange failed.")
+
+    qc.draw("mpl")
